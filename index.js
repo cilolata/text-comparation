6e63f7863e191ec5b0176e215896869b22f041e0
cilolata <cilolata@gmail.com>
2020-08-30 16:39:24 -0300

    first commit

--- index.js
+++ index.js
@@ -2,19 +2,28 @@ const btn = document.querySelector("#comparar");
 
 btn.addEventListener("click", function(event){
     event.preventDefault();
+    const form = document.getElementById("form");
     const text1 = document.getElementById("text1").value;
+    const text1Lower = text1.toLowerCase()
     const text2 = document.getElementById("text2").value;
-
-    if(text1.length === text2.length){
-         const comparacao = text1.localeCompare(text2)
-         console.log(comparacao)
-
-    }else{
-        console.log("texto incompleto")
+    const text2Lower = text2.toLowerCase()
+    
+    
+    if( text1Lower === text2Lower){
+        const res = "O texto combina";
+        const div = document.getElementById("resultado");        
+        div.innerText = res ;
+    } else {
+        const res = "texto incompleto ou não combina";
+        const div = document.getElementById("resultado");        
+        div.innerText = res ;
     }
 
-     
-
+  
 
 });
+
+
+
+
 
